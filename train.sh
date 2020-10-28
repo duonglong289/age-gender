338e6b9447d66f8aa5e8e4e1fce8536864f30ff3
root <ronaldocr9803@gmail.com>
2020-10-28 10:43:33 +0000

    change dataset path

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python3 train.py \
-    --dataset=dataset/all_faces \
+    --dataset=../all_faces \
     --num_epochs=50 \
     --batch_size=64 \
     --init_lr=0.002 \
-    --num_workers=8 \
+    --num_workers=4 \
     --logs="./logs"
