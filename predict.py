3f83999b00a3eed8e3a1eb2b201a993cd822881b
root <ronaldocr9803@gmail.com>
2020-10-29 08:29:47 +0000

    add predict for multiple images

--- predict.py
+++ predict.py
@@ -12,11 +12,10 @@ def main():
     model.init_model()
     model.device = torch.device("cpu")
     # state_dict = torch.load(torch.load(state_dict_path), map_location=self.device)
-    # import ipdb; ipdb.set_trace()
-    state_dict_path = "./logs/3_0.54209313364256_gender_0.0_age.pt"
-    model.model.load_state_dict(torch.load(state_dict_path), strict=False)
-    model.model.eval().to(model.device)
-    # model.load_statedict(state_dict_path="./logs/3_0.54209313364256_gender_0.0_age.pt")
+    state_dict_path = "./logs/2_0.5528364550499689_gender_197.11904907226562_age.pt"
+    # model.model.load_state_dict(torch.load(state_dict_path), strict=False)
+    # model.model.eval().to(model.device)
+    model.load_statedict(state_dict_path="./logs/2_0.5528364550499689_gender_197.11904907226562_age.pt")
     # print(model)
     return model
 
@@ -24,7 +23,6 @@ def pre_process_img(img):
     normalize_transform = T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     transform = T.Compose([
                 T.ToPILImage(),
-                # T.Resize([self.image_size,self.image_size]),
                 T.Resize([224,224]),
                 T.ToTensor(),
                 normalize_transform])
@@ -33,16 +31,39 @@ def pre_process_img(img):
 
 if __name__ == "__main__":
     import argparse
+    import os
+    import random
+    import glob
     parser = argparse.ArgumentParser(description="Training Liveness Detection")
-    parser.add_argument("--image", type=str, default = "../all_faces/00000A02G0.png", required=False, help="Path to image")
+    #default = "../all_faces/00000A02G0.png"
+    parser.add_argument("--image", type=str, required=False, help="Path to image")
+    age_range = ["0-5", "5-10", "10-14", "14-18", "18-21", "21-25", "25-29",
+                 "25-29", "29-34", "34-38", "38-42", "42-46",
+                 "46-50", "50-55", "55-60" ,"60-65", ">= 65"]
 
     args = parser.parse_args()
-    image = cv2.imread(args.image)
-    image = pre_process_img(image)
-    # import ipdb; ipdb.set_trace()
-    model = main()
-    # import ipdb; ipdb.set_trace()
-    output = model.predict_image(image.unsqueeze(0))
-    import ipdb; ipdb.set_trace()
-    # print(model.predict_image(torch.from_numpy(image)))
+    if not args.image:
+        lst_image = random.sample(glob.glob(os.path.join("../all_faces", "*")),10)
+    else: 
+        lst_image = [args.image]
+    data= []
+    for image_path in lst_image:
+    # file_result = open("MyFile.txt","a")
+    # file_result.write("a\n")
+        image = cv2.imread(image_path)
+        image = pre_process_img(image)
+        model = main()
+        _, age, gender = model.predict_image(image.unsqueeze(0))
+        gender = torch.argmax(torch.exp(gender)).detach().numpy().item()
+        age = age_range[torch.argmax(torch.exp(age)).detach().numpy().item()]
+        # print("{}   {}   {}   {}".format(args.image.split("A")[1].split(".")[0].split("G")[0], \
+        #                                 args.image.split("A")[1].split(".")[0].split("G")[1], age, gender))
+        data.append([image_path.split("A")[1].split(".")[0].split("G")[0], \
+                    age,
+                    image_path.split("A")[1].split(".")[0].split("G")[1], \
+                    gender])
+    import pandas as pd
+    df = pd.DataFrame(data, columns=['true_age', 'true_gender', 'pred_age', 'pred_gender'])
+    df.index = pd.to_numeric(df.index, errors='coerce')
+    print(df)
     
