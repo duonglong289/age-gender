e835e40702512bc287288834a930ce396642015b
michaeltiat <thanhhai@techainer.com>
2021-06-19 22:50:47 +0700

    michael commit

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 python3 train.py \
     --model_name=mobilenet_v2 \
     --widen_factor=1 \
-    --dataset=dataset/small_data \
+    --dataset=dataset/last_face_age_gender \
     --num_epochs=50 \
-    --batch_size=128 \
-    --init_lr=0.002 \
+    --batch_size=64 \
+    --init_lr=0.000125 \
     --num_workers=8 \
     --logs="./logs/log_" \
     --num_age_classes=81
